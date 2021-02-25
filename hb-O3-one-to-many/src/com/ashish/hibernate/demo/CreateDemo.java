d1620a5e74b3a0e518730612a67be9375b9649ff
yuvrajmahalle <aashishmahalle90@gmail.com>
2021-02-25 20:40:28 +0530

    Added demo implementation of association one-to-many mapping

---
+++ hb-O3-one-to-many/src/com/ashish/hibernate/demo/CreateDemo.java
@@ -0,0 +1,50 @@
+package com.ashish.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ashish.hibernate.demo.entity.Course;
+import com.ashish.hibernate.demo.entity.Instructor;
+import com.ashish.hibernate.demo.entity.InstructorDetail;
+import com.ashish.hibernate.demo.entity.Student;
+
+public class CreateDemo {
+
+	public static void main(String[] args) {
+		 
+		// create session factory
+		// SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class).buildSessionFactory();
+		SessionFactory factory = new Configuration()
+									.configure()
+									.addAnnotatedClass(Instructor.class)
+									.addAnnotatedClass(InstructorDetail.class)
+									.addAnnotatedClass(Course.class)
+									.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			 Instructor tempInstructor = new Instructor("Susain","YT","test1@test.com");
+			 
+			 InstructorDetail tempInstructorDetail = new InstructorDetail("youtube.com/tester","testing");
+			 
+			 tempInstructor.setInstructorDetail(tempInstructorDetail);
+		 
+			session.beginTransaction();
+			
+			
+			// this will also save tempInstructor because of cascadeType.ALL
+			session.save(tempInstructor);
+		 
+			session.getTransaction().commit();
+			 
+			
+		}finally {
+			factory.close();
+		}
+
+	}
+
+}
---
+++ hb-O3-one-to-many/src/com/ashish/hibernate/demo/CreateInstructorDemo.java
@@ -0,0 +1,51 @@
+package com.ashish.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ashish.hibernate.demo.entity.Course;
+import com.ashish.hibernate.demo.entity.Instructor;
+import com.ashish.hibernate.demo.entity.InstructorDetail;
+import com.ashish.hibernate.demo.entity.Student;
+
+public class CreateInstructorDemo {
+
+	public static void main(String[] args) {
+		 
+		// create session factory
+		// SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class).buildSessionFactory();
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Instructor.class)
+									.addAnnotatedClass(InstructorDetail.class)
+									.addAnnotatedClass(Course.class)
+									.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+			session.beginTransaction();
+			
+			int theId = 1;
+			Instructor tempInstructor = session.get(Instructor.class, theId);
+			
+			System.out.println("tempInstructor :: " + tempInstructor);
+			
+			System.out.println("Course :: " + tempInstructor.getCourses());
+			 
+			session.getTransaction().commit();
+			System.out.println("Done!!");
+			
+		}catch(Exception exe){
+			exe.printStackTrace();
+		}finally {
+			session.close();
+			factory.close();
+		}
+
+	}
+
+}
